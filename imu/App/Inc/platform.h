ab4f9adddc3d12725d68aa92aae28ee91a17c08d
Evan Wu <ewu43951@gmail.com>
2026-04-01 10:51:12 -0500

    Defined the platform specific imu read/write functions

---
+++ imu/App/Inc/platform.h
@@ -0,0 +1,32 @@
+
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+
+#include "stdint.h"
+
+
+
+/// @brief Platform specific imu write HAL function
+/// @param handle handle to imu context struct
+/// @param reg register address
+/// @param bufp pointer to write buffer
+/// @param len length of data to be written
+/// @return 
+int32_t platform_write_imu(void* handle, uint8_t reg, const uint8_t* bufp, uint16_t len);
+
+
+/// @brief Platform specific imu read HAL function
+/// @param handle handle to imu context struct
+/// @param reg register address
+/// @param bufp pointer to read buffer
+/// @param len length of data to be read
+/// @return 
+int32_t platform_read_imu(void* handle, uint8_t reg, uint8_t* bufp, uint16_t len);
+
+
+
+
+
+
+#endif // PLATFORM_H
---
+++ imu/App/Src/platform.c
@@ -0,0 +1,15 @@
+
+#include "../Inc/platform.h"
+
+
+int32_t platform_write_imu(void *handle, uint8_t reg, const uint8_t *bufp, uint16_t len)
+{
+    return 0;
+}
+
+
+
+int32_t platform_read_imu(void *handle, uint8_t reg, uint8_t *bufp, uint16_t len)
+{
+    return 0;
+}
